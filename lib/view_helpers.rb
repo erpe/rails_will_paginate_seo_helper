bcbf7be72391c81954a52db745a088188db74145
rene paulokat <rene@so36.net>
2016-09-09 18:09:56 +0200

    Removes 'page=1' from first page-link

--- lib/view_helpers.rb
+++ lib/view_helpers.rb
@@ -3,34 +3,50 @@ module RailsWillPaginateSeoHelper
     def will_paginate_seo_links(collection)
       return unless collection.respond_to?(:current_page)
       @collection = collection
-      build_tags.html_safe 
+      build_tags
     end
 
     private
 
-
     def build_tags
       if next_link_tag && previous_link_tag
-        return previous_link_tag << next_link_tag
+        return (previous_link_tag << next_link_tag).html_safe
       end
-      return next_link_tag if next_link_tag
-      return previous_link_tag if previous_link_tag
-    end
-
-    def next_link_tag
-      next_page = @collection.next_page
-      if next_page
-        next_page_url = request.original_url.gsub(/page\=\d{1,}/, "page=#{next_page}")
-        return tag(:link, href: next_page_url, rel: :next)
-      end
+      return previous_link_tag.html_safe if previous_link_tag
+      next_link_tag.html_safe if next_link_tag
     end
 
     def previous_link_tag
       previous_page = @collection.previous_page
+      original_url = request.original_url
+
       if previous_page
-        prev_page_url = request.original_url.gsub(/page\=\d{1,}/, "page=#{previous_page}")
+        if previous_page == 1
+          prev_page_url = original_url.gsub(/(&|\?)page\=\d{1,}/, '')
+        else
+          prev_page_url = original_url.gsub(/page\=\d{1,}/, "page=#{previous_page}")
+        end
         return tag(:link, href: prev_page_url, rel: :prev)
       end
+    end
+
+    def next_link_tag
+      next_page = @collection.next_page
+      original_url = request.original_url
+
+      match = original_url.match(/(&|\?)page\=\d{1,}/)
+
+      if match          # has :page url-parameter
+        next_page_url = original_url.gsub(/page\=\d{1,}/, "page=#{next_page}")
+      else              # no :page url-parameter page
+        sign_match = original_url.match(/\?/)
+        if sign_match   # url has parameter attached
+          next_page_url = original_url.concat("&page=#{next_page}")
+        else            # url has no parameter attached
+          next_page_url = original_url.concat("?page=#{next_page}")
+        end
+      end
+      tag(:link, href: next_page_url, rel: :next) if next_page_url
     end
   end
 end
--- rails_will_paginate_seo_helper.gemspec
+++ rails_will_paginate_seo_helper.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'rails_will_paginate_seo_helper'
-  s.version = '0.1.1'
+  s.version = '0.1.3'
   s.date = '2016-09-09'
   s.summary = 'SEO ViewHelper for will_paginate'
   s.description = 'adds ability to generate seo link-tags for will_paginate pages'
