d8682f912f9b49b3aee617459d2f8fd07fca99ca
rene paulokat <rene@so36.net>
2016-09-09 16:54:32 +0200

    wip

--- lib/view_helpers.rb
+++ lib/view_helpers.rb
@@ -3,10 +3,19 @@ module RailsWillPaginateSeoHelper
     def will_paginate_seo_links(collection)
       return unless collection.respond_to?(:current_page)
       @collection = collection
-      (previous_link_tag << next_link_tag).html_safe
+      build_tags.html_safe 
     end
 
     private
+
+
+    def build_tags
+      if next_link_tag && previous_link_tag
+        return previous_link_tag << next_link_tag
+      end
+      return next_link_tag if next_link_tag
+      return previous_link_tag if previous_link_tag
+    end
 
     def next_link_tag
       next_page = @collection.next_page
@@ -14,7 +23,6 @@ module RailsWillPaginateSeoHelper
         next_page_url = request.original_url.gsub(/page\=\d{1,}/, "page=#{next_page}")
         return tag(:link, href: next_page_url, rel: :next)
       end
-      ''
     end
 
     def previous_link_tag
@@ -23,7 +31,6 @@ module RailsWillPaginateSeoHelper
         prev_page_url = request.original_url.gsub(/page\=\d{1,}/, "page=#{previous_page}")
         return tag(:link, href: prev_page_url, rel: :prev)
       end
-      ''
     end
   end
 end
